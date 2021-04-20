9e01a505aae03904e098d4acd74e67030a630cb0
Dhananjay Kaushik <dhananjay@centilytics.com>
2021-04-21 02:52:04 +0530

    changes done

--- portfolio-three-js/src/app/components/home/home.component.ts
+++ portfolio-three-js/src/app/components/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { GUI } from 'dat.gui';
 import { Configuration } from 'src/app/classes/Configuration';
+import { IGuiControls } from 'src/app/interfaces/IGuiControls';
 import {
     BoxGeometry,
     Mesh,
     MeshNormalMaterial,
     PerspectiveCamera,
     Scene,
-    TextureLoader,
+    TorusBufferGeometry,
+    Vector3,
     WebGLRenderer,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
@@ -17,83 +20,187 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
     styleUrls: ['./home.component.sass'],
 })
 export class HomeComponent implements OnInit {
+    scene: Scene;
+    camera: PerspectiveCamera = null;
+    renderer: WebGLRenderer = null;
+    orbitControls: OrbitControls = null;
+    datGui: GUI = null;
+    material: MeshNormalMaterial = null;
+
+    guiControls: IGuiControls = {
+        totalElements: 1000,
+        wireframe: false,
+        cameraPosition: new Vector3(0, 0, 4),
+    };
+    sceneMeshIds: number[] = [];
+
     constructor() {}
 
     ngOnInit(): void {
+        // Creating Debugger
+        this.createDebugger();
+
+        // Creating basic layout
+        this.createBasicLayout();
+
+        // Creating elements
+        this.createElements();
+
+        // Events Handling
+        this.eventHandling();
+
+        // Handling controls
+        this.setControls();
+
         /**
-         * Camera
+         * Game Loop
          */
-        const camera = new PerspectiveCamera(
+        this.startGameLoop();
+    }
+
+    /**
+     * Functions
+     */
+
+    createBasicLayout() {
+        // Adding Camera
+        this.camera = new PerspectiveCamera(
             Configuration.FOV,
             this.aspect,
             Configuration.NEAR,
             Configuration.FAR
         );
+        this.updateCameraPosition();
 
-        /**
-         * Scene
-         */
-        const scene = new Scene();
-        scene.add(camera);
+        // Adding Scene
+        this.scene = new Scene();
+        this.scene.add(this.camera);
 
-        const textureLoader = new TextureLoader();
-        const boxImage = textureLoader.load('../../../assets/box.png');
-
-        /**
-         * Box Geometry
-         */
-        for (let i = 0; i < 1000; ++i) {
-            const geometry = new BoxGeometry(0.1, 0.1, 0.1);
-            const material = new MeshNormalMaterial();
-            const cube = new Mesh(geometry, material);
-
-            const posX = (Math.random() - 0.5) * 10;
-            const posY = (Math.random() - 0.5) * 10;
-            const posZ = (Math.random() - 0.5) * 10;
-
-            cube.position.set(posX, posY, posZ);
-
-            scene.add(cube);
-        }
-
-        camera.position.z = 4;
-        /**
-         * Renderer
-         */
-        const renderer = new WebGLRenderer({
+        // Adding Renderer
+        this.renderer = new WebGLRenderer({
             canvas: document.querySelector('.webgl') as HTMLCanvasElement,
             antialias: true,
         });
-        renderer.setSize(this.width, this.height);
+        this.renderer.setSize(this.width, this.height);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+    }
 
-        /**
-         * Events Handling
-         */
-        const eventHandling = () => {
-            window.addEventListener('resize', () => {
-                renderer.setSize(this.width, this.height);
-                camera.aspect = this.aspect;
-                camera.updateProjectionMatrix();
-            });
-        };
-        eventHandling();
+    updateCameraPosition() {
+        this.camera.position.set(
+            this.guiControls.cameraPosition.x,
+            this.guiControls.cameraPosition.y,
+            this.guiControls.cameraPosition.z
+        );
 
+        // Camera position debugger
+        const cameraFolder = this.datGui.addFolder('Camera Position');
+        cameraFolder
+            .add(this.camera.position, 'x')
+            .min(-80)
+            .max(80)
+            .step(1)
+            .name('X');
+        cameraFolder
+            .add(this.camera.position, 'y')
+            .min(-80)
+            .max(80)
+            .step(1)
+            .name('Y');
+        cameraFolder
+            .add(this.camera.position, 'z')
+            .min(-80)
+            .max(80)
+            .step(1)
+            .name('Z');
+        cameraFolder.close();
+    }
+
+    eventHandling() {
+        // TODO: Try fromEvent
+        window.addEventListener('resize', () => {
+            this.renderer.setSize(this.width, this.height);
+            this.camera.aspect = this.aspect;
+            this.camera.updateProjectionMatrix();
+        });
+    }
+
+    createElements() {
+        if (this.sceneMeshIds && this.sceneMeshIds.length) {
+            this.removeSceneMeshes();
+        }
+        const boxGeometry = new BoxGeometry(0.4, 0.4, 0.4);
+        const donutGeometry = new TorusBufferGeometry(0.3, 0.2, 20, 45);
+        this.material = new MeshNormalMaterial();
+        this.material.wireframe = this.guiControls.wireframe;
+
+        for (let i = 0; i < this.guiControls.totalElements; ++i) {
+            const mesh = new Mesh(
+                Math.random() > 0.5 ? boxGeometry : donutGeometry,
+                this.material
+            );
+            let posX = (Math.random() - 0.5) * 30;
+            let posY = (Math.random() - 0.5) * 30;
+            let posZ = (Math.random() - 0.5) * 30;
+            this.sceneMeshIds.push(mesh.id);
+            mesh.position.set(posX, posY, posZ);
+            this.scene.add(mesh);
+        }
+    }
+
+    removeSceneMeshes() {
+        this.sceneMeshIds.forEach((meshId) => {
+            const element = this.scene.getObjectById(meshId);
+            if (element) {
+                this.scene.remove(element);
+            }
+        });
+    }
+
+    setControls() {
         /**
          * Orbit Control
          */
-        const orbitControl = new OrbitControls(camera, renderer.domElement);
+        this.orbitControls = new OrbitControls(
+            this.camera,
+            this.renderer.domElement
+        );
+    }
 
-        /**
-         * Game Loop
-         */
+    startGameLoop() {
         const tick = () => {
             window.requestAnimationFrame(tick);
-            orbitControl.update();
-            renderer.render(scene, camera);
+            this.orbitControls.update();
+            this.renderer.render(this.scene, this.camera);
         };
         tick();
     }
 
+    createDebugger() {
+        this.datGui = new GUI({
+            name: 'Configuration',
+        });
+        this.datGui
+            .add(this.guiControls, 'totalElements')
+            .name('Total Elements')
+            .min(1)
+            .max(1000)
+            .onFinishChange(() => {
+                this.createElements();
+            });
+
+        this.datGui
+            .add(this.guiControls, 'wireframe')
+            .onFinishChange((value) => {
+                if (this.material) {
+                    this.material.wireframe = value;
+                }
+            })
+            .name('Wireframe');
+    }
+
+    /**
+     * Getters and Setters
+     */
     get width(): number {
         return window.innerWidth;
     }
---
+++ portfolio-three-js/src/app/interfaces/IGuiControls.ts
@@ -0,0 +1,7 @@
+import { Vector3 } from 'three';
+
+export interface IGuiControls {
+    totalElements: number;
+    wireframe: boolean;
+    cameraPosition: Vector3;
+}
