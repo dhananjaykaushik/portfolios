e42ca3fe2503793f8684ed8c8262833fd39c4742
Dhananjay Kaushik <dhananjay@centilytics.com>
2021-04-26 19:15:48 +0530

    changes done

--- portfolio-three-js/src/app/components/projects/projects.component.ts
+++ portfolio-three-js/src/app/components/projects/projects.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { IProjectData } from 'src/app/interfaces/IProjectData';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.sass']
+    selector: 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.sass'],
 })
 export class ProjectsComponent implements OnInit {
+    projects: IProjectData[] = [];
 
-  constructor() { }
+    constructor() {
+        // this.projects = new Array(50).fill(1).map((value, index) => {
+        //     return {
+        //         projectName: `Project ${index + 1}`,
+        //     };
+        // });
+    }
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
